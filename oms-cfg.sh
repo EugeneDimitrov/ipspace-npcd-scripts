8d13c9c0133393c074641d62875d7c12bbeb49e5
EugeneDimitrov <eugene.dimitrov@gmail.com>
2021-04-16 19:22:15 +0500

    Update oms-cfg.sh

--- oms-cfg.sh
+++ oms-cfg.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 wget https://raw.githubusercontent.com/EugeneDimitrov/ipspace-npcd-scripts/main/95-omsagent.conf
-sudo chown omsagent:omiusers 95-omsagent.conf
-sudo chmod 644 95-omsagent.conf
-sudo mv 95-omsagent.conf /etc/rsyslog.d/
+chown omsagent:omiusers 95-omsagent.conf
+chmod 644 95-omsagent.conf
+mv 95-omsagent.conf /etc/rsyslog.d/
